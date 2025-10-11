b3bb86397157c3136045703baf7a55c02add7c0c
adzafic <adnan.dzafic21@gmail.com>
2025-10-11 18:13:02 +0200

    Utilizamod un array de usuarios para mostrar las tarjetas Añadimos mas margenes en las css de la aplicacion

--- projects/01-twitter-card/src/App.jsx
+++ projects/01-twitter-card/src/App.jsx
@@ -2,14 +2,36 @@ import "./App.css";
 import { TwitterFollowCard } from "./TwitterFollowCard";
 
 export function App() {
+  const users = [
+    {
+      userName: "adzafic",
+      name: "Adnan Dzafic",
+      isFollowing: false,
+    },
+    {
+      userName: "ginusharakelyan",
+      name: "Ginush Arakelyan",
+      isFollowing: true,
+    },
+    {
+      userName: "Javicci",
+      name: "Javier Gracia",
+      isFollowing: true,
+    },
+  ];
   return (
-    <>
-      <TwitterFollowCard userName="adzafic" initialIsFollowing={false}>
-        <strong>Adnan Dzafic</strong>
-      </TwitterFollowCard>
-      <TwitterFollowCard userName="ginusharakelyan" initialIsFollowing>
-        <strong>Ginush Arakelyan</strong>
-      </TwitterFollowCard>
-    </>
+    <section className="App">
+      {users.map((user) => {
+        const { userName, name, isFollowing } = user;
+        return (
+          <TwitterFollowCard
+            userName={userName}
+            initialIsFollowing={isFollowing}
+          >
+            <strong>{name}</strong>
+          </TwitterFollowCard>
+        );
+      })}
+    </section>
   );
 }
