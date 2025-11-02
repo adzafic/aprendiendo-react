fd65a3c36624ec9a422fcf7dd421a29507a70fa3
adzafic <adnan.dzafic21@gmail.com>
2025-11-02 11:38:17 +0100

    refactor: separar el useMovies en useSearch y Use movie en 2 hooks

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -34,14 +34,41 @@ function Movies({ listMovies }) {
   );
 }
 
-//todo: separar en hooks
+function useSearch() {
+  const [search, setSearch] = useState("");
+  const [error, setError] = useState(null);
+  const isFirstRender = useRef(true);
+
+  useEffect(() => {
+    console.log("useEffect search", { search });
+    console.log("first Render", isFirstRender.current);
+    if (isFirstRender.current) {
+      isFirstRender.current = search === "";
+      return;
+    }
+    if (search === "") {
+      setError("No se puede buscar una pelicula vacia");
+      return;
+    }
+    if (search.match(/^\d+$/)) {
+      setError("No se puede buscar una pelicula con solo numeros");
+      return;
+    }
+    if (search.length < 3) {
+      setError("La busqueda debe tener al menos 3 caracteres");
+      return;
+    }
+    setError(null);
+  }, [search]);
+  return { search, setSearch, error };
+}
+
 function useMovies({ searchMovie }) {
-  const [search, setSearch] = useState({ searchMovie });
   const [listMovies, setListMovies] = useState([]);
   useEffect(() => {
     const movieUrl = `http://www.omdbapi.com/?i=tt3896198&apikey=${
       import.meta.env.VITE_OMDB_API_KEY
-    }&s=${search.searchMovie}`;
+    }&s=${searchMovie}`;
     fetch(movieUrl)
       .then((response) => response.json())
       .then((data) => {
@@ -55,12 +82,13 @@ function useMovies({ searchMovie }) {
         });
         setListMovies(mapedMovies);
       });
-  }, [search]);
-  return { listMovies, setSearch };
+  }, [searchMovie]);
+  return { listMovies };
 }
 
 function App() {
-  const { listMovies, setSearch } = useMovies({ searchMovie: "avengers" });
+  const { search, setSearch, error } = useSearch();
+  const { listMovies } = useMovies({ searchMovie: search });
   const searchInputRef = useRef();
 
   const handleSubmit = function (event) {
@@ -74,11 +102,13 @@ function App() {
     const fields = Object.fromEntries(new FormData(event.target));
     const value = searchInputRef.current.value;
     console.log(value, fields.search);
-    setSearch({ searchMovie: fields.search });
+
+    //setSearch({ searchMovie: fields.search });
+    setSearch(fields.search);
   };
   const handleChange = function (event) {
     const fields = Object.fromEntries(new FormData(event.target.form));
-    setSearch({ searchMovie: fields.search });
+    setSearch(fields.search);
   };
 
   return (
@@ -88,12 +118,17 @@ function App() {
         <form action="" onSubmit={handleSubmit}>
           <label htmlFor="busqueda">Busca tu pelicula</label>
           <input
-            ref={searchInputRef}
             onChange={handleChange}
             name="search"
             type="text"
             placeholder="Avengers, Star Wars, The Matrix..."
+            ref={searchInputRef}
           />
+          {error && (
+            <div>
+              <p style={{ color: "red" }}>{error}</p>
+            </div>
+          )}
           <button type="submit">Buscar</button>
         </form>
       </header>
